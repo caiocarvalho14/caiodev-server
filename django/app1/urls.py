7f69bd946a423123d6b1c13fcc4b2d29870d299a
Caio Carvalho <k4io12345@gmail.com>
2026-07-06 08:26:26 -0300

    uptime auto update:django

--- django/app1/urls.py
+++ django/app1/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import include, path, include
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('uptime', views.uptime, name='uptime')
 ]
--- django/app1/views.py
+++ django/app1/views.py
@@ -14,3 +14,7 @@ def index(request):
         models.cliques.objects.create()
     
     return render(request, 'index.html', {'total_cliques': total_cliques, "started_at": SERVER_STARTED_AT.__format__("%H:%M:%S"), "uptime_seconds": int(uptime.total_seconds())})
+
+def uptime(request):
+    uptime = datetime.now(timezone.utc) - SERVER_STARTED_AT
+    return JsonResponse({'uptime': int(uptime.total_seconds())})
